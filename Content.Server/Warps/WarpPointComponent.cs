37441989d8ddd60e3c79bf006fe77c6706d62b0d
Kevin Zheng <kevinz5000@gmail.com>
2022-10-23 12:49:02 -0700

    Add Warper component and system
    Warpers teleport players to a named WarpPoint on any map when applied.
    They can be used to implement portals and traversal between floors.

--- Content.Server/Warps/WarpPointComponent.cs
+++ Content.Server/Warps/WarpPointComponent.cs
@@ -6,6 +6,10 @@ namespace Content.Server.Warps
     [RegisterComponent]
     public sealed class WarpPointComponent : Component
     {
+        /// Unique (across all loaded maps) identifier for teleporting to warp points.
+        [ViewVariables(VVAccess.ReadWrite)] [DataField("id")] public string? ID { get; set; }
+
+        /// Readable name for ghost warp points.
         [ViewVariables(VVAccess.ReadWrite)] [DataField("location")] public string? Location { get; set; }
 
         /// <summary>
