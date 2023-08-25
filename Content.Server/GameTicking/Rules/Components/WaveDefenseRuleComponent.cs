b502cd8caf693d1f73a261ee803d852a55dc4363
Checkraze <71046427+Cheackraze@users.noreply.github.com>
2023-08-25 19:00:53 -0400

    wave_defense_august (#108)
    * wave balance
    
    * its set to easy mode so

--- Content.Server/GameTicking/Rules/Components/WaveDefenseRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/WaveDefenseRuleComponent.cs
@@ -9,7 +9,7 @@ public sealed class WaveDefenseRuleComponent : Component
     public float DifficultyMod = 0.65f;
 
     [DataField("waveTime")]
-    public int WaveTime = 210;
+    public int WaveTime = 240;
 
     [DataField("greetingSound", customTypeSerializer: typeof(SoundSpecifierTypeSerializer))]
     public SoundSpecifier? GreetSound = new SoundPathSpecifier("/Audio/Misc/nukeops.ogg");
