ae87b26fec62e6f94f314dd3e05cc1f546fde031
PatoGrone <134236674+PatoGrone@users.noreply.github.com>
2024-06-09 05:18:39 -0300

    minigun, explosives and pump/bolting

---
+++ Content.Shared/Input/CMKeyFunctions.cs
@@ -0,0 +1,19 @@
+﻿using Robust.Shared.Input;
+
+namespace Content.Shared._CM14.Input;
+
+[KeyFunctions]
+public sealed class CMKeyFunctions
+{
+    public static readonly BoundKeyFunction MappingUnselect = "MappingUnselect";
+    public static readonly BoundKeyFunction SaveMap = "SaveMap";
+    public static readonly BoundKeyFunction MappingEnablePick = "MappingEnablePick";
+    public static readonly BoundKeyFunction MappingEnableDelete = "MappingEnableDelete";
+    public static readonly BoundKeyFunction MappingPick = "MappingPick";
+    public static readonly BoundKeyFunction MappingRemoveDecal = "MappingRemoveDecal";
+    public static readonly BoundKeyFunction MappingCancelEraseDecal = "MappingCancelEraseDecal";
+    public static readonly BoundKeyFunction MappingOpenContextMenu = "MappingOpenContextMenu";
+
+    public static readonly BoundKeyFunction CMPumpShotgun = "CMPumpShotgun";
+    public static readonly BoundKeyFunction CMXenoWideSwing = "CMXenoWideSwing";
+}
