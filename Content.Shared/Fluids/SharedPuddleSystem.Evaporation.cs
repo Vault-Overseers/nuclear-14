df2ddbe0cc652f72d1488054e28c72fa50e118b8
Peptide90 <78795277+Peptide90@users.noreply.github.com>
2024-07-04 10:21:25 +0100

    QoL update 2 (#281)
    * saws and some timber changes
    
    * fix meat cooking
    
    * bonfire cooking attempt 1
    
    * fix stove and bonfire cooking at the expense of bounding boxes
    
    * tool id fix
    
    * makes dirty and irradiated water plus wasterblood evaporate.
    
    * fixes satchels and duffels for tribals and townspeople 1
    
    * stops trash and puddles spawning at 0,0 on round start
    
    * potential fix for announcements / bluespace locker / unused events.
    
    * fix deathclaw attack sound and animation
    
    * fix gate smoothing
    
    * new event announcement sounds
    
    * fix radstag steak cooking
    
    * remove bonfire power need
    
    * test for different layers of tables.
    
    * fix generator bounding boxes and increase explosion
    
    * adds timed enemy spawners
    
    * make timber into biggers stacks for crafting
    
    * spawner fix

--- Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
+++ Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
@@ -7,8 +7,14 @@ public abstract partial class SharedPuddleSystem
 {
     [ValidatePrototypeId<ReagentPrototype>]
     private const string Water = "Water";
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string WaterDirty = "WaterDirty";
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string WaterIrradiated = "WaterIrradiated";
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string WastelandBlood = "WastelandBlood";
 
-    public static readonly string[] EvaporationReagents = [Water];
+    public static readonly string[] EvaporationReagents = [Water, WaterDirty, WaterIrradiated, WastelandBlood];
 
     public bool CanFullyEvaporate(Solution solution)
     {
