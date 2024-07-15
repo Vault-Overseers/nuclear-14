5e8e87ce8c7118e8f85bf7183da61c85855fb1cb
ScyronX <166930367+ScyronX@users.noreply.github.com>
2024-07-15 13:12:37 +0300

    adds a short doafter to hypospray for when you use it on others (#358)
    adds hypospray doafter

---
+++ Content.Shared/Chemistry/Components/HyposprayComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.Serialization;
+using Content.Shared.DoAfter;
+
+namespace Content.Shared.Chemistry.Components;
+
+[Serializable, NetSerializable]
+public sealed partial class HyposprayDoAfterEvent : SimpleDoAfterEvent
+{
+}
