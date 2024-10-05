0b92e0c335922e3f070953f148db0b19f654f30d
VMSolidus <evilexecutive@gmail.com>
2024-10-05 09:21:18 -0400

    Fix Mood (Revert PR#564) (#566)
    Fix Mood

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2446,13 +2446,16 @@ namespace Content.Shared.CCVar
         #region Mood System
 
         public static readonly CVarDef<bool> MoodEnabled =
-            CVarDef.Create("mood.enabled", false, CVar.SERVER);
+            CVarDef.Create("mood.enabled", true, CVar.SERVER);
 
         public static readonly CVarDef<bool> MoodIncreasesSpeed =
             CVarDef.Create("mood.increases_speed", true, CVar.SERVER);
 
         public static readonly CVarDef<bool> MoodDecreasesSpeed =
             CVarDef.Create("mood.decreases_speed", true, CVar.SERVER);
+
+        public static readonly CVarDef<bool> MoodModifiesThresholds =
+            CVarDef.Create("mood.modify_thresholds", false, CVar.SERVER);
 
         #endregion
 
