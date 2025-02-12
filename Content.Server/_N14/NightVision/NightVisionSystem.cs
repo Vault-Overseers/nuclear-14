43025f2713e890aa1f1398df7b6784680c752da8
VMSolidus <evilexecutive@gmail.com>
2025-02-12 00:24:00 -0500

    The entire N14 Nightvision folder going away because this is now entirely upstream in a more maintained way.

--- Content.Server/_N14/NightVision/NightVisionSystem.cs
+++
@@ -1,5 +0,0 @@
-using Content.Shared._N14.NightVision;
-
-namespace Content.Server._N14.NightVision;
-
-public sealed class NightVisionSystem : SharedNightVisionSystem;
--- Content.Shared/_N14/NightVision/NightVisionItemVisuals.cs
+++
@@ -1,9 +0,0 @@
-﻿using Robust.Shared.Serialization;
-
-namespace Content.Shared._N14.NightVision;
-
-[Serializable, NetSerializable]
-public enum NightVisionItemVisuals
-{
-    Active,
-}
