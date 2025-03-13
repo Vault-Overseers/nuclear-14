441feb9a10ca396a6c183fa9888e045494568cd7
Peptide90 <User94636@protonmail.com>
2025-03-13 21:34:14 +0000

    Miscellaneous Fixes From White Dream (#1911)
    Ports the following PRs:
    https://github.com/WWhiteDreamProject/wwdpublic/pull/271
    https://github.com/WWhiteDreamProject/wwdpublic/pull/282
    https://github.com/WWhiteDreamProject/wwdpublic/pull/255
    https://github.com/WWhiteDreamProject/wwdpublic/pull/254
    https://github.com/WWhiteDreamProject/wwdpublic/pull/246
    https://github.com/WWhiteDreamProject/wwdpublic/pull/223
    https://github.com/WWhiteDreamProject/wwdpublic/pull/220
    https://github.com/WWhiteDreamProject/wwdpublic/pull/219
    https://github.com/WWhiteDreamProject/wwdpublic/pull/213
    
    This is a rather huge variety of fixes to be honest.
    
    :cl: White Dream
    - fix: Fixed airlock opening and closing visuals
    - fix: Fixed Weather spamming infinite sounds.
    - fix: Cockroaches no longer spam organs when stepped on.
    - fix: Fixed the timer on the Return To Round button
    - fix: Fixed Return to Round not remembering how long it's been since
    you died if you re-enter your body.
    - fix: Fixed harpy flight visuals not showing for other people.
    - fix: Fixed disabler bolts not going through windows like other kinds
    of lasers.
    - fix: Fixed Vim Harness being free
    - fix: Fixed handcuffs not respecting your arm length.
    - fix: Made tending wounds not as message spammy.
    - fix: Fixed shuttle windows not preventing you from being electrocuted
    by grills under them.
    
    ---------
    
    Co-authored-by: vanx <61917534+Vaaankas@users.noreply.github.com>
    Co-authored-by: vanx <discord@vanxxxx>
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    Co-authored-by: Repo <47093363+Titian3@users.noreply.github.com>

--- Content.Client/Weather/WeatherSystem.cs
+++ Content.Client/Weather/WeatherSystem.cs
@@ -49,7 +49,6 @@ public sealed class WeatherSystem : SharedWeatherSystem
             return;
 
         weather.Stream ??= _audio.PlayGlobal(weatherProto.Sound, Filter.Local(), true)?.Entity;
-
         if (!TryComp(weather.Stream, out AudioComponent? comp))
             return;
 
