4c7fe336de9eb77a4084be40c83330d09c896c77
Peptide90 <78795277+Peptide90@users.noreply.github.com>
2025-07-09 00:22:44 +0100

    Fix fusion core system and update visuals

--- Content.Server/Power/Generator/FusionCoreFuelGeneratorAdapterSystem.cs
+++ Content.Server/Power/Generator/FusionCoreFuelGeneratorAdapterSystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Power.Generator;
 using Robust.Shared.Containers;
+using Robust.Server.GameObjects;
 
 namespace Content.Server.Power.Generator;
 
@@ -10,13 +11,31 @@ namespace Content.Server.Power.Generator;
 public sealed partial class FusionCoreFuelGeneratorAdapterSystem : EntitySystem
 {
     [Dependency] private readonly ItemSlotsSystem _slots = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
     public override void Initialize()
     {
         base.Initialize();
+        SubscribeLocalEvent<FusionCoreFuelGeneratorAdapterComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<FusionCoreFuelGeneratorAdapterComponent, EntInsertedIntoContainerMessage>(OnContainerModified);
+        SubscribeLocalEvent<FusionCoreFuelGeneratorAdapterComponent, EntRemovedFromContainerMessage>(OnContainerModified);
+
         SubscribeLocalEvent<FusionCoreFuelGeneratorAdapterComponent, GeneratorGetFuelEvent>(OnGetFuel);
         SubscribeLocalEvent<FusionCoreFuelGeneratorAdapterComponent, GeneratorUseFuel>(OnUseFuel);
         SubscribeLocalEvent<FusionCoreFuelGeneratorAdapterComponent, GeneratorEmpty>(OnEmpty);
+    }
+
+    private void OnMapInit(EntityUid uid, FusionCoreFuelGeneratorAdapterComponent comp, MapInitEvent args)
+    {
+        UpdateAppearance(uid, comp);
+    }
+
+    private void OnContainerModified(EntityUid uid, FusionCoreFuelGeneratorAdapterComponent comp, ContainerModifiedMessage args)
+    {
+        if (args.Container.ID != comp.SlotId)
+            return;
+
+        UpdateAppearance(uid, comp);
     }
 
     private void OnGetFuel(EntityUid uid, FusionCoreFuelGeneratorAdapterComponent comp, ref GeneratorGetFuelEvent args)
@@ -33,7 +52,19 @@ public sealed partial class FusionCoreFuelGeneratorAdapterSystem : EntitySystem
     private void OnEmpty(EntityUid uid, FusionCoreFuelGeneratorAdapterComponent comp, GeneratorEmpty args)
     {
         if (_slots.TryGetSlot(uid, comp.SlotId, out var slot))
-            _slots.TryEject(uid, slot);
+        {
+            _slots.TryEject(uid, slot, null, out _);
+            UpdateAppearance(uid, comp);
+        }
+    }
+
+    private void UpdateAppearance(EntityUid uid, FusionCoreFuelGeneratorAdapterComponent comp)
+    {
+        if (!TryComp(uid, out AppearanceComponent? appearance))
+            return;
+
+        var hasCore = _slots.TryGetSlot(uid, comp.SlotId, out var slot) && slot.Item != null;
+        _appearance.SetData(uid, GeneratorVisuals.HasCore, hasCore, appearance);
     }
 }
 
