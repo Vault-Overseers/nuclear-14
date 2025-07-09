78aaa3f67c6a040435428c97a1bb6ff38023ca31
Peptide90 <78795277+Peptide90@users.noreply.github.com>
2025-07-09 16:45:09 +0100

    Implement functional roulette table (#920)
    * Add functional roulette table
    
    * Update casino.yml
    
    * make roulette table use chat system for markdown as well as popup

---
+++ Content.Server/_N14/Casino/RouletteTableSystem.cs
@@ -0,0 +1,63 @@
+using Content.Shared._N14.Casino;
+using Content.Shared.Interaction;
+using Content.Server.Chat.Managers;
+using Content.Shared.Chat;
+using Content.Shared.Popups;
+using Robust.Shared.Player;
+using Robust.Shared.Utility;
+using Robust.Server.GameObjects;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Server._N14.Casino;
+
+public sealed class RouletteTableSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IChatManager _chat = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<RouletteTableComponent, ActivateInWorldEvent>(OnActivate);
+    }
+
+    private void OnActivate(EntityUid uid, RouletteTableComponent comp, ActivateInWorldEvent args)
+    {
+        if (args.Handled || comp.Active)
+            return;
+
+        args.Handled = true;
+        comp.Active = true;
+        UpdateAppearance(uid, comp);
+
+        Timer.Spawn(TimeSpan.FromSeconds(comp.SpinTime), () => FinishSpin(uid));
+    }
+
+    private void FinishSpin(EntityUid uid)
+    {
+        if (!TryComp(uid, out RouletteTableComponent? comp))
+            return;
+
+        comp.Active = false;
+        UpdateAppearance(uid, comp);
+
+        var number = _random.Next(0, 37);
+        var color = number == 0 ? Loc.GetString("roulette-color-green") :
+            _random.Prob(0.5f) ? Loc.GetString("roulette-color-red") : Loc.GetString("roulette-color-black");
+
+        var message = Loc.GetString("roulette-result", ("number", number), ("color", color));
+        var plain = FormattedMessage.RemoveMarkupPermissive(message);
+        var filter = Filter.Pvs(uid, 4f);
+        _chat.ChatMessageToManyFiltered(filter, ChatChannel.Server, plain, message, uid, false, true, null);
+        _popup.PopupEntity(plain, uid, filter, true);
+    }
+
+    private void UpdateAppearance(EntityUid uid, RouletteTableComponent comp)
+    {
+        var state = comp.Active ? RouletteTableState.On : RouletteTableState.Off;
+        _appearance.SetData(uid, RouletteTableVisuals.State, state);
+    }
+}
