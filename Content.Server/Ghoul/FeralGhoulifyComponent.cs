2ec9aa60dc3b55d47f8f94062745caa180e6de95
Peptide90 <78795277+Peptide90@users.noreply.github.com>
2025-07-10 21:20:17 +0100

    Add ghoulification system (#907)

---
+++ Content.Server/Ghoul/FeralGhoulifyComponent.cs
@@ -0,0 +1,34 @@
+namespace Content.Server.Ghoul;
+
+/// <summary>
+/// Applied to ghouls. Excessive radiation causes them to go feral.
+/// </summary>
+[RegisterComponent]
+public sealed partial class FeralGhoulifyComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float AccumulatedRads;
+
+    [DataField]
+    public float Threshold = 100f;
+
+    [DataField]
+    public float ProbabilityPerRad = 0.02f;
+
+    [DataField]
+    public float NextNotify = 40f;
+
+    /// <summary>
+    /// Once this amount of radiation is reached, others will see the mob
+    /// twitching when examined.
+    /// </summary>
+    [DataField]
+    public float ExamineThreshold = 40f;
+
+    /// <summary>
+    /// Amount of radiation lost per second when not irradiated.
+    /// </summary>
+    [DataField]
+    public float DecayPerSecond = 0.25f;
+
+}
