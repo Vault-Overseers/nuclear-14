16c42838e269cad8bbe26ad7be108a5c1cf03eea
TheDoktorZ <z.public.storage@gmail.com>
2025-07-10 13:40:16 +0200

    Small Addition
    This is more or less a lie, anyhow the PR description shall have all you need to know.

--- Content.Shared/CCVar/CCVars.SoftCrit.cs
+++ Content.Shared/CCVar/CCVars.SoftCrit.cs
@@ -10,10 +10,10 @@ public sealed partial class CCVars
     ///     you still can't stand up while crit, and you're still more or less helpless.
     /// </summary>
     public static readonly CVarDef<bool> AllowMovementWhileCrit =
-        CVarDef.Create("mobstate.allow_movement_while_crit", true, CVar.REPLICATED);
+        CVarDef.Create("mobstate.allow_movement_while_crit", false, CVar.REPLICATED);
 
     public static readonly CVarDef<bool> AllowTalkingWhileCrit =
-        CVarDef.Create("mobstate.allow_talking_while_crit", true, CVar.REPLICATED);
+        CVarDef.Create("mobstate.allow_talking_while_crit", false, CVar.REPLICATED);
 
     /// <summary>
     ///     Currently does nothing because I would have to figure out WHERE I would even put this check, and the mover controller is fairly complicated.
--- Resources/Locale/en-US/npc/factions.ftl
+++ Resources/Locale/en-US/npc/factions.ftl
@@ -1,1 +1,3 @@
 npcfaction-component-faction-set = Faction set to: {$faction}
+npc-order-start = "Fire at will!"
+npc-order-stop = "Hold your fire!"
