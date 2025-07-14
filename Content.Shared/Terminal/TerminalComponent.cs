7111261e247210229b42f4f81d9c3d6f5cca368e
Peptide90 <78795277+Peptide90@users.noreply.github.com>
2025-07-14 15:30:43 +0100

    Add terminal cartridge system (#934)
    * Add basic terminal system with cartridge support
    
    * Fix TerminalMenu inheritance
    
    * Fix TerminalMenu root and add missing using
    
    * Fix TerminalMenu tag and public names
    
    * Theme terminals like pipboy
    
    * Fix terminal style font color
    
    * update terminals

---
+++ Content.Shared/Terminal/TerminalComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Terminal;
+
+/// <summary>
+/// Marker component for computer terminals that use cartridge programs.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class TerminalComponent : Component
+{
+}
