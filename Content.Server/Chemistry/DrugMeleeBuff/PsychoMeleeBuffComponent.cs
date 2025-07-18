41af7790db4ce61995006a8bbe7a10b22761e244
Peptide90 <78795277+Peptide90@users.noreply.github.com>
2025-07-18 13:16:38 +0100

    Add server stub for Psycho melee buff

---
+++ Content.Server/Chemistry/DrugMeleeBuff/PsychoMeleeBuffComponent.cs
@@ -0,0 +1,10 @@
+
+namespace Content.Server.Chemistry.DrugMeleeBuff;
+
+/// <summary>
+///     Placeholder server-side component for Psycho melee buff.
+/// </summary>
+[RegisterComponent]
+public sealed partial class PsychoMeleeBuffComponent : Component
+{
+}
