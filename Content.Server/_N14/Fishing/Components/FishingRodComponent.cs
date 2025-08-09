76778fe40c5beb5ee92006400ffcadb590ecaf4c
Peptide90 <User94636@protonmail.com>
2025-08-09 21:52:53 +0100

    remove N14 fishing and add goonfishing to tiles. TODO: Need to do rods and fish update

--- Content.Server/_N14/Fishing/Components/FishingRodComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server._N14.Fishing.Components;
-
-[RegisterComponent]
-public sealed partial class FishingRodComponent : Component
-{
-    [DataField("castTime")]
-    public float CastTime = 3f;
-}
