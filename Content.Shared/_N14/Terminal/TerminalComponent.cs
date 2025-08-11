91a46d26aba5c9fa3f2e4150b7352c63b9a97d7c
Peptide90 <User94636@protonmail.com>
2025-08-11 19:32:34 +0100

    Add terminal cartridge system (#934)
    * Add basic terminal system with cartridge support
    
    * Fix TerminalMenu inheritance
    
    * Fix TerminalMenu root and add missing using
    
    * Fix TerminalMenu tag and public names
    
    * Theme terminals like pipboy
    
    * Fix terminal style font color
    
    * update terminals
    
    (cherry picked from commit f07e1af68ff09d3722b68979456f7415a6f6f91d)

---
+++ Content.Shared/_N14/Terminal/TerminalComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._N14.Terminal;
+
+/// <summary>
+/// Marker component for computer terminals that use cartridge programs.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class TerminalComponent : Component
+{
+}
---
+++ Content.Shared/_N14/Terminal/TerminalUiKey.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._N14.Terminal;
+
+[Serializable, NetSerializable]
+public enum TerminalUiKey
+{
+    Key
+}
